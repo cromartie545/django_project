92789deff1ea93776a3893c87fc6fcb17cabb194
cromartie1020 <hrc245@mail.com>
2026-01-22 23:57:48 -0500

    Post added to blog model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -24,4 +24,61 @@ class Employee(models.Model):
         db_table = "employee"
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f'{self.first_name} {self.last_name}'
+
+class Student(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name  = models.CharField(max_length = 30)
+    email      = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+        
+    title = models.CharField(max_length=250)
+    slug  = models.SlugField(max_length=250)
+    body  = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+    status =models.CharField(max_length = 2, choices = Status, default = Status.DRAFT)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE,
+                               related_name='blog_posts')
+    class Meta:
+        ordering = ('-publish',)
+        db_table = 'title'
+    indxs = [
+        models.Index(fields=['-publish'])
+    ]
+
+    def __str__(self):
+        return self.title
+    
+
+
+
+class FavouritePost(models.Model):
+    pk = models.CompositePrimaryKey(
+        'user', 'post'
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete= models.CASCADE
+    )
+    
+    post = models.ForeignKey(
+        'blog.Post',
+        on_delete= models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add = True)
+    
+
+
+
+
+
--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +30,6 @@ urlpatterns = [
     path('', include('blog.urls')),
     
 ]
+
+if settings.DEBUG:
+    print('This is a test.')
