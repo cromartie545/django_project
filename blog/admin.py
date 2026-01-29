3f1178e79c28a64bd70366f312152772efe9e4f1
cromartie1020 <hrc245@mail.com>
2026-01-28 23:18:35 -0500

    added a custom manager

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,14 @@ from .models import Post,Employee
 @admin.register(Post)
 class PostAdmiin(admin.ModelAdmin):
     list_display = ['title','slug','body','status']
+    search_fields = ['title','body']
+    prepopulated_fields ={'slug':('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy ='publish'
+    ordering =['status','publish']
+
 
 @admin.register(Employee)    
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display=['first_name', 'last_name','email'] 
+    list_display=['first_name', 'last_name','email']
+    search_fields = ['last_name'] 
